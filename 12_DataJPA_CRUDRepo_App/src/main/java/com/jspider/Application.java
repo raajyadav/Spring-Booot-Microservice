64baf56ef3a7a5a63438d2f9cea7337db1c7f4cd
Raj Yadav <raajyadavnp@gmail.com>
2025-11-24 02:39:54 +0530

    Custom Query Completed

--- 12_DataJPA_CRUDRepo_App/src/main/java/com/jspider/Application.java
+++ 12_DataJPA_CRUDRepo_App/src/main/java/com/jspider/Application.java
@@ -1,5 +1,8 @@
 package com.jspider;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.boot.SpringApplication;
@@ -25,17 +28,88 @@ public class Application {
 
 //	System.out.println(repo.getClass().getName());
 
-	Book book = new Book();
-	book.setBookId(103);
-	book.setBookName("Spring");
-	book.setBookPrice(344.89);
-	
-	repo.save(book);
-	
-	System.out.println("Record Inserted.....");
+		Book book1 = new Book();
+		book1.setBookId(102);
+		book1.setBookName("CSS");
+		book1.setBookPrice(2000.00);
+
+		Book book2 = new Book();
+		book2.setBookId(105);
+		book2.setBookName("React-Native");
+		book2.setBookPrice(2600.00);
+
+//	List<Book> bookList = new ArrayList<>();
+//	bookList.add(book1);
+//	bookList.add(book2);
+//	repo.saveAll(bookList);
+//	
+
+//    repo.saveAll(Arrays.asList(book1,book2)); /* to insert multiple object */
+//
+//		System.out.println("Record Inserted.....");
 
 //		Optional<Book> findById = repo.findById(102);
 //		System.out.println(findById.get());
+
+//		boolean status = repo.existsById(101); /* to check the presence of data by ID */
+//		System.out.println("Presence :: "+ status);
+
+//		Long count = repo.count(); /* It count Total Record in the Table*/
+//		System.out.println("Total Data is: "+ count);
+
+//		Optional<Book> findById = repo.findById(103);
+//		if(findById.isPresent())
+//			System.out.println(findById.get());
+//		else
+//			System.out.println("Not Such Record in Table");
+//		
+//	   Iterable<Book> findAllById = repo.findAllById(Arrays.asList(101,102,103));
+//	
+//	   for(Book b:findAllById) {
+//		System.out.println(b);
+//	   }
+
+//	   Iterable<Book> findAll =repo.findAll();
+//	 
+//	   for(Book b:findAll) {
+//		   System.out.println(b);
+//	   }
+
+//		repo.deleteById(305); /* For delete */
+//		if(repo.existsById(204)) {
+//			repo.deleteById(205);
+//		}else {
+//			System.out.println("No record Found");
+//		}
+//		
+// 	repo.deleteAllById(Arrays.asList(102,103));
+
+		// FINDBY METHOD
+
+//		List<Book> findByBookPriceGreaterThan = repo.findByBookPriceGreaterThan(2000.0);
+//		for(Book b:findByBookPriceGreaterThan) {
+//			System.out.println(b);
+//		}
+
+//	List<Book> findByBookIdGreaterThan = 	repo.findByBookIdGreaterThan(103);
+//	for(Book b:findByBookIdGreaterThan) {
+//		System.out.println(b);
+//	}
+		
+//	List<Book> findByBookName =	repo.findByBookName("Java");
+//	for(Book b:findByBookName) {
+//		System.out.println(b);
+//	}
+		
+//	List<Book> getAllBooks = repo.getAllBooks();
+//	for(Book b:getAllBooks) {
+//		System.out.println(b);
+//	}
+		
+		List<Book> getBooks = repo.getBooks();
+		for(Book b:getBooks) {
+			System.out.println(b);
+		}
 
 	}
 
--- 12_DataJPA_CRUDRepo_App/src/main/java/com/jspider/repository/BookRepository.java
+++ 12_DataJPA_CRUDRepo_App/src/main/java/com/jspider/repository/BookRepository.java
@@ -1,9 +1,28 @@
 package com.jspider.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.jspider.entity.Book;
 
 public interface BookRepository extends CrudRepository<Book, Integer>{
 
+	public List<Book> findByBookPriceGreaterThan(Double price);
+	
+	public List<Book> findByBookIdGreaterThan(Integer id );
+	
+	public List<Book> findByBookName(String name);
+	
+	//SQL QUERY
+	@Query(value = "select * from book", nativeQuery=true)
+	public List<Book> getAllBooks();
+	
+	//HQL QUERY
+	@Query("from Book")
+	public List<Book> getBooks();
+	
+	
+	
 }
