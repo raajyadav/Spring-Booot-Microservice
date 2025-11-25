da9acaac66a7d27d4514739a9745c502d85a5ba1
Raj Yadav <raajyadavnp@gmail.com>
2025-11-26 00:55:19 +0530

    MVC App1

---
+++ 16_Web_MVC_App1/src/main/java/com/jspider/controller/MsgController.java
@@ -0,0 +1,36 @@
+package com.jspider.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MsgController {
+	
+	@GetMapping("/welcome")
+	public ModelAndView getWelcomeMsg() {
+		
+			ModelAndView mav = new ModelAndView();
+			
+			mav.addObject("msg", "Hi, Welcome to Qspider");
+			
+			mav.setViewName("message");
+			
+			return mav;
+			
+	}
+	
+	@GetMapping("/greet")
+	public ModelAndView getGreetMsg() {
+		
+		ModelAndView mav = new ModelAndView();
+		
+		mav.addObject("msg", "Good Evening");
+		
+		mav.setViewName("message");
+		
+		return mav;
+		
+	}
+
+}
---
+++ 16_Web_MVC_App1/src/main/webapp/views/message.jsp
@@ -0,0 +1,6 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<body>
+    <h1>${msg}</h1>
+</body>
+</html>
